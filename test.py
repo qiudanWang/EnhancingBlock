ff794fe20d5af5d783f0e455bf711ad1fbd6656a
qiudanWang <32563967+qiudanWang@users.noreply.github.com>
2021-06-23 00:00:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ import net
 from function import adaptive_instance_normalization, coral
 
 
-def test_transform(size, crop):
+def img_transform(size, crop):
     transform_list = []
     if size != 0:
         transform_list.append(transforms.Resize(size))
@@ -24,51 +24,25 @@ def test_transform(size, crop):
     return transform
 
 
-def style_transfer(vgg, decoder, content, style, normal_vector, constant, alpha=1.0,
-                   interpolation_weights=None):
-    assert (0.0 <= alpha <= 1.0)
-
+def style_transfer(vgg, decoder, content, style, normal_vector, constant, lamda=1.0):
     content_f = vgg(content)
-
     style_f = vgg(style)
-
-
-    if interpolation_weights:
-        _, C, H, W = content_f.size()
-        feat = torch.FloatTensor(1, C, H, W).zero_().to(device)
-        base_feat = adaptive_instance_normalization(content_f, style_f)
-        for i, w in enumerate(interpolation_weights):
-            feat = feat + w * base_feat[i:i + 1]
-        content_f = content_f[0:1]
-    else:
-        feat = adaptive_instance_normalization(content_f, style_f)
-
+    feat = adaptive_instance_normalization(content_f, style_f)
     distance = torch.mm(feat.reshape(1, 524288), torch.transpose(normal_vector, 1, 0)) + constant
-
     if (distance > 0):
-      feat = feat * alpha + content_f * (1 - alpha) + distance  * normal_vector.reshape(1, 512, 32, 32)
-    else:
-      feat = feat * alpha + content_f * (1 - alpha)
-
+      feat = feat + lamda * distance  * normal_vector.reshape(1, 512, 32, 32)
     res = decoder(feat)
-
     return decoder(feat)
 
 
 parser = argparse.ArgumentParser()
 # Basic options
-parser.add_argument('--content', type=str,
-                    help='File path to the content image')
 parser.add_argument('--content_dir', type=str,
                     help='Directory path to a batch of content images')
-parser.add_argument('--style', type=str,
-                    help='File path to the style image, or multiple style \
-                    images separated by commas if you want to do style \
-                    interpolation or spatial control')
 parser.add_argument('--style_dir', type=str,
                     help='Directory path to a batch of style images')
 parser.add_argument('--vgg', type=str, default='models/vgg_normalised.pth')
-parser.add_argument('--decoder', type=str, default='models/decoder.pth')
+parser.add_argument('--decoder', type=str, default='./models/decoder_iter_160000.pth')
 
 # Additional options
 parser.add_argument('--content_size', type=int, default=512,
@@ -87,12 +61,9 @@ parser.add_argument('--output', type=str, default='output',
 # Advanced options
 parser.add_argument('--preserve_color', action='store_true',
                     help='If specified, preserve color of the content image')
-parser.add_argument('--alpha', type=float, default=1.0,
+parser.add_argument('--lamda', type=float, default=1.0,
                     help='The weight that controls the degree of \
                              stylization. Should be between 0 and 1')
-parser.add_argument(
-    '--style_interpolation_weights', type=str, default='',
-    help='The weight for blending the style of multiple style images')
 parser.add_argument('--normal_vector', type=str,
                     help='File path to the normal vector')
 parser.add_argument('--constant', type=str,
@@ -102,36 +73,21 @@ parser.add_argument('--artist', type=str,
 
 args = parser.parse_args()
 
-do_interpolation = False
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 output_dir = Path(args.output)
 output_dir.mkdir(exist_ok=True, parents=True)
 
-# Either --content or --contentDir should be given.
-assert (args.content or args.content_dir)
-if args.content:
-    content_paths = [Path(args.content)]
-else:
-    content_dir = Path(args.content_dir)
-    content_paths = [f for f in content_dir.glob('*')]
+# Either --contentDir should be given.
+assert (args.content_dir)
+content_dir = Path(args.content_dir)
+content_paths = [f for f in content_dir.glob('*')]
 
-# Either --style or --styleDir should be given.
-assert (args.style or args.style_dir)
-if args.style:
-    style_paths = args.style.split(',')
-    if len(style_paths) == 1:
-        style_paths = [Path(args.style)]
-    else:
-        do_interpolation = True
-        assert (args.style_interpolation_weights != ''), \
-            'Please specify interpolation weights'
-        weights = [int(i) for i in args.style_interpolation_weights.split(',')]
-        interpolation_weights = [w / sum(weights) for w in weights]
-else:
-    style_dir = Path(args.style_dir)
-    style_paths = [f for f in style_dir.glob('*')]
+# Either --styleDir should be given.
+assert (args.style_dir)
+style_dir = Path(args.style_dir)
+style_paths = [f for f in style_dir.glob('*')]
 
 decoder = net.decoder
 vgg = net.vgg
@@ -146,40 +102,25 @@ vgg = nn.Sequential(*list(vgg.children())[:31])
 vgg.to(device)
 decoder.to(device)
 
-content_tf = test_transform(args.content_size, args.crop)
-style_tf = test_transform(args.style_size, args.crop)
+content_tf = img_transform(args.content_size, args.crop)
+style_tf = img_transform(args.style_size, args.crop)
 
 normal_vector = torch.from_numpy(np.load(args.normal_vector)).to(device)
 constant = torch.from_numpy(np.load(args.constant)).to(device)
 
 for content_path in content_paths:
-    if do_interpolation:  # one content image, N style image
-        style = torch.stack([style_tf(Image.open(str(p))) for p in style_paths])
-        content = content_tf(Image.open(str(content_path))) \
-            .unsqueeze(0).expand_as(style)
-        style = style.to(device)
-        content = content.to(device)
+    for style_path in style_paths:
+        content = content_tf(Image.open(str(content_path)))
+        style = style_tf(Image.open(str(style_path)))
+        if args.preserve_color:
+            style = coral(style, content)
+        style = style.to(device).unsqueeze(0)
+        content = content.to(device).unsqueeze(0)
         with torch.no_grad():
-            output = style_transfer(vgg, decoder, content, style,
-                                    args.alpha, interpolation_weights)
+            output = style_transfer(vgg, decoder, content, style, normal_vector, constant,
+                                    args.lamda)
         output = output.cpu()
-        output_name = output_dir / '{:s}_interpolation{:s}'.format(
-            content_path.stem, args.save_ext)
-        save_image(output, str(output_name))
 
-    else:  # process one content and one style
-        for style_path in style_paths:
-            content = content_tf(Image.open(str(content_path)))
-            style = style_tf(Image.open(str(style_path)))
-            if args.preserve_color:
-                style = coral(style, content)
-            style = style.to(device).unsqueeze(0)
-            content = content.to(device).unsqueeze(0)
-            with torch.no_grad():
-                output = style_transfer(vgg, decoder, content, style, normal_vector, constant,
-                                        args.alpha)
-            output = output.cpu()
-
-            output_name = output_dir / '{:s}_{:s}_stylized_{:s}{:s}'.format(
-                content_path.stem, style_path.stem.replace(" ", ""), args.artist, args.save_ext)
-            save_image(output, str(output_name))
+        output_name = output_dir / '{:s}_{:s}_stylized_{:s}{:s}'.format(
+            content_path.stem, style_path.stem.replace(" ", ""), args.artist, args.save_ext)
+        save_image(output, str(output_name))     
