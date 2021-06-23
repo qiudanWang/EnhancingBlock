5c8e883ab000f0fd2b4d738349c2f466317d9d4b
qiudanWang <32563967+qiudanWang@users.noreply.github.com>
2021-06-24 00:00:11 +0800

    Update feat.py

--- feat.py
+++ feat.py
@@ -1,4 +1,4 @@
-import argparse                                                                                                                                                                                                                               
+import argparse
 from pathlib import Path
 
 import torch
@@ -8,7 +8,7 @@ from torchvision import transforms
 
 import net
 
-def img_transform():
+def image_transform():
     transform_list = [
         transforms.Resize(size=(512, 512)),
         transforms.RandomCrop(256),
@@ -17,8 +17,8 @@ def img_transform():
     return transforms.Compose(transform_list)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--img_dir", type=str, required=True,
-                    help="directory of the img images")
+parser.add_argument("--image_dir", type=str, required=True,
+                    help="directory of the image images")
 parser.add_argument("--output_dir", type=str, required=True,
                     help="directory of the output latent code")
 parser.add_argument('--vgg', type=str, required=False,
@@ -33,20 +33,20 @@ vgg.load_state_dict(torch.load("./models/vgg_normalised.pth"))
 vgg = nn.Sequential(*list(vgg.children())[:31])
 network = net.Net(vgg, decoder)
 
-img_dir = Path(args.img_dir)
-img_paths = [f for f in img_dir.glob('*')]
-img_tf = img_transform()
-print("Number of img images: %d" % len(img_paths))
+image_dir = Path(args.image_dir)
+image_paths = [f for f in image_dir.glob('*')]
+image_tf = image_transform()
+print("Number of image images: %d" % len(image_paths))
 
 output_dir = Path(args.output_dir)
 output_dir.mkdir(exist_ok=True, parents=True)
 
-for img_path in img_paths:
-    print("handle image: %s" % img_path.name)
-    img = img_tf(Image.open(str(img_path)))
-    img = img.to(device).unsqueeze(0)
-    img_feats = network.encode_with_intermediate(img)
+for image_path in image_paths:
+    print("handle image: %s" % image_path.name)
+    image = image_tf(Image.open(str(image_path)))
+    image = image.to(device).unsqueeze(0)
+    image_feats = network.encode_with_intermediate(image)
 
-    output_name = img_path.name.replace(".jpg", ".pth")
+    output_name = image_path.name.replace(".jpg", ".pth")
     output_file = output_dir / output_name
-    torch.save(img_feats[-1], output_file)
+    torch.save(image_feats[-1], output_file)
