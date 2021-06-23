9948b67be7cdfcf1a043809c330329969613e65e
qiudanWang <32563967+qiudanWang@users.noreply.github.com>
2021-06-23 23:59:08 +0800

    Update feat.py

--- feat.py
+++ feat.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse                                                                                                                                                                                                                               
 from pathlib import Path
 
 import torch
@@ -17,8 +17,8 @@ def img_transform():
     return transforms.Compose(transform_list)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--content_dir", type=str, required=True,
-                    help="directory of the content images")
+parser.add_argument("--img_dir", type=str, required=True,
+                    help="directory of the img images")
 parser.add_argument("--output_dir", type=str, required=True,
                     help="directory of the output latent code")
 parser.add_argument('--vgg', type=str, required=False,
@@ -33,21 +33,20 @@ vgg.load_state_dict(torch.load("./models/vgg_normalised.pth"))
 vgg = nn.Sequential(*list(vgg.children())[:31])
 network = net.Net(vgg, decoder)
 
-content_dir = Path(args.content_dir)
-content_paths = [f for f in content_dir.glob('*')]
-content_tf = img_transform()
-print("Number of style images: %d" % len(content_paths))
+img_dir = Path(args.img_dir)
+img_paths = [f for f in img_dir.glob('*')]
+img_tf = img_transform()
+print("Number of img images: %d" % len(img_paths))
 
 output_dir = Path(args.output_dir)
 output_dir.mkdir(exist_ok=True, parents=True)
 
-for content_path in content_paths:
-    print("handle image: %s" % style_path.name)
-    content = content_tf(Image.open(str(content_path)))
-    content = content.to(device).unsqueeze(0)
-    content_feats = network.encode_with_intermediate(content)
+for img_path in img_paths:
+    print("handle image: %s" % img_path.name)
+    img = img_tf(Image.open(str(img_path)))
+    img = img.to(device).unsqueeze(0)
+    img_feats = network.encode_with_intermediate(img)
 
-    output_name = content_path.name.replace(".jpg", ".pth")
+    output_name = img_path.name.replace(".jpg", ".pth")
     output_file = output_dir / output_name
-    torch.save(content_feats[-1], output_file)
-
+    torch.save(img_feats[-1], output_file)
